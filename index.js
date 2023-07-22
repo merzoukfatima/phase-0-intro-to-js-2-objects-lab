df7f4b3a3e2db65efa8edb850d71b57c23aa9b8e
MerzoukFatimaZohra <merzoukfatimazahraa@gmail.com>
2023-07-22 03:10:25 +0100

    Assignement Completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Write your solution in this file!
+const employee = {
+    name:"Fatima",
+    streetAddress:"Pretty View"
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    return { ...employee,
+        [key]:value,
+    }
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key]=value
+    return employee
+}
+
+function deleteFromEmployeeByKey(employee, key){
+     const new_employee={...employee}
+     delete new_employee[key]
+     return new_employee
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+     delete employee[key]
+     return employee
+}
+
